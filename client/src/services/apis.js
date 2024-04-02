dddaf615419923c77ae8ae660490265f63da900e
Dheeraj Sharma <113750315+DheerajSharma22@users.noreply.github.com>
2024-04-02 16:59:32 +0530

    Update apis.js

--- client/src/services/apis.js
+++ client/src/services/apis.js
@@ -1,4 +1,4 @@
-const BASE_URL = "https://study-notion-backend-three.vercel.app"
+const BASE_URL = "https://study-notion-backend-three.vercel.app/api/v1"
 
 // AUTH ENDPOINTS
 export const endpoints = {
